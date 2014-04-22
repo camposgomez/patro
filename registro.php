41d4b9689ee573e2d4e0d1c0bcfa707f2cf699a1
desarrollo <camposgomez@yahoo.com>
2014-04-21 20:57:42 -0500

    cambio de registra con la e

--- registro.php
+++ registro.php
@@ -21,7 +21,7 @@ require_once 'php/conexion.php';
 				<li><A href="indexlog.php" >Principal</A></li>
 			
 				<li><A href="cargar.php" >Administraci&oacute;n</A></li>
-				<li><A href="contactenos.php" >Contact</A></li>
+				<li><A href="contactenos.php" >Contacte</A></li>
 			</ul>
 		</nav>
 		<section id="seccion">
