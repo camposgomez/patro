e311624816172d665ef7a5bf4e6c8e81d98c9165
desarrollo <camposgomez@yahoo.com>
2014-04-27 17:38:24 -0500

    contactenos de loquesea.com

--- contactenos.php
+++ contactenos.php
@@ -1,78 +1,60 @@
 <?php
-require_once 'php/conexion.php';
+    require_once 'php/conexion.php';
 ?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
-
-	<meta charset="utf-8"/>
-	<title>Loquesea.com</title>
-	<link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title>Loquesea.com</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
 <body>
-	<div id="agrupar">
-		<header id="cabecera">
-			Loquesea.com		
-		</header>
-		<nav id="menu">
-			<ul>
-
-				<li><A href="indexlog.php" >Principal</A></li>
-				<li><A href="cargar.php" >Administraci&oacute;n</A></li>
-				<li><A href="contactenos.php" >Contacto</A></li>
-			</ul>
-		</nav>
-		<section id="seccion">
-			<h1>Contactenos</h1>
-			<article id="registro">
-			<form method="POST" action="registracontacto.php"> 	
-                            <p><label>Asunto:&nbsp;&nbsp;</label> <input class="textbox" type="text" name="asunto" class="cajas" />  </p>
-				<p><label>Nombre:&nbsp;</label> <input class="textbox" type="text" name="nombre" class="cajas" />  </p>
-				<p><label>E-mail:&nbsp;&nbsp;&nbsp;</label> <input class="textbox" type="email" name="email" class="cajas" />  </p>
-				<p><label>Telefono:&nbsp;</label> <input class="textbox" type="tel" name="telefono" class="cajas" />  </p>
-				<p><label>Detalle:&nbsp&nbsp&nbsp</label> <input class="textbox" type="text" name="detalle" class="cajas"/> </p>
-				<button type="submit" class="btn">Enviar</button>
-			</form>
-                        </article>
-		</section>
-		<aside id="columna">
-			<figure>
-				<img src="img/contactenos.jpg">
-			</figure>
-		</aside>		
-                <section id="seccion1">
-                    <div class="CSSTableGenerator" >
-                    <?php
-                        $obj_conex=new Conexion();
-                        $sql="select * from contactenos";
-                        $datos=$obj_conex->get_resultados_query($sql);
-                        $control=0;
-                        if (! $datos){
-                           echo "La consulta SQL contiene errores.";
-                           exit();
-                        }else {
-                            echo "<table><tr><td>CodiComent</td><td>Asunto</td><td>Nombre</td><td>email</td><td>Telefono</td><td>Detalle</td><td>Borrar</td>
-                                 </tr><tr>";
-                        //obtenemos los datos resultado de la consulta
-                            foreach ($datos as $fila){
-                                        $control=$control+1;
-                                        echo "<td>".$fila["codicont"]."</td>
-                                      <td>".$fila["asunto"]."</td><td>".$fila["nombre"]."</td><td>".$fila["email"]."</td><td>".$fila["telefono"]."</td><td>".$fila["detalle"]."</td><td><a href='borracontactenos.php?codicont=".$fila["codicont"]."'>Borrar</a></td>";
-                                           echo "</tr><tr>"; 
-                                           $control=1;
-                                     }
-                                    echo "</tr></table>";
-                                }
-                    ?>
-                </section>
-		<footer id="pie">
-			<small>Derechos Reservados &copy; 2014 </small>
-			<address>
-				<a href=Ã¢ÂÂhttp://www.loquesea.comÃ¢ÂÂ>Carlos Campos & Felipe Ruiz </a>
-			</address>
-		</footer>
+	<div class="container">
+		<div class="col-md-12">
+			<header id="cabecera">
+				Loquesea.com		
+			</header>
+		</div>	
 	</div>
+    <div class="container">
+        <div class="collapse navbar-collapse navbar-ex1-collapse" >
+            <ul class="nav navbar-nav" >
+                <li><a href="indexlog.php">Principal</a>
+                </li>
+                <li><a href="cargar.php">Administraci&oacute;n</a>
+                </li>
+                <li><a href="contactenos.php">Contacto</a>
+                </li>
+            </ul>
+        </div>
+	</div>
+    <div class="container">
+    	<div class="col-md-9">
+
+    	</div>
+    	<div class="col-md-3">
+	        <div class="list-group" >
+            	<a href="calificaciones.php" class="list-group-item">Calificaciones</a>
+            	<a href="estados.php" class="list-group-item">Estados</a>
+            	<a href="registro.php" class="list-group-item">Usuarios</a>
+            	<a href="cargar.php" class="list-group-item">Articulos</a>
+        	</div>
+    	</div>
+    </div>
+
+    <div class="container">
+        <hr>
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Carlos Campos & Felipe Ruiz </a>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
